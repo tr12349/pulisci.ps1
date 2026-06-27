79a9c8053e37d20cd67ee0c0c5b915cf58666b72
tr12349 <pippohaker@gmail.com>
2026-06-27 14:59:29 +0200

    Update clean_temp.ps1

--- clean_temp.ps1
+++ clean_temp.ps1
@@ -1,4 +1,4 @@
-unction Invoke-Module {
+function Invoke-Module {
 
     Remove-Item "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
     Remove-Item "C:\Windows\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
